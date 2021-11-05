7904436f8ce472ee7d72d7c7020f26071dcd6726
marco-tav <marco.tavella@outlook.com>
2021-11-05 12:23:56 -0300

    Change while loop to for loop

--- scripts/script.js
+++ scripts/script.js
@@ -3,11 +3,8 @@ let amount = n**2;
 
 const etchASketch = document.querySelector('.etch-a-sketch');
 
-let i = 0;
-
-while (i < amount) {
+for (let i = 0; i < amount; i++) {
     const cell = document.createElement('div');
     cell.setAttribute('class', 'cell');
     etchASketch.appendChild(cell);
-    i++;
 }
