c314308048b15c832807f151e9b075217fb860fe
marco-tav <marco.tavella@outlook.com>
2021-11-05 15:01:43 -0300

    Add hover event so that cells of the grid change color on hover

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,19 @@
 let n = 16;
 let amount = n**2;
 
-const gridCointainer = document.querySelector('grid-container');
+const gridContainer = document.querySelector('.grid-container');      
 
 for (let i = 0; i < amount; i++) {
     const cell = document.createElement('div');
     cell.setAttribute('class', 'cell');
     gridContainer.appendChild(cell);
 }
+
+const cells = document.querySelectorAll('.cell');
+
+cells.forEach((cell) => {
+
+    cell.addEventListener('mouseover', () => {
+        cell.classList.add('drawing');
+    })
+})
