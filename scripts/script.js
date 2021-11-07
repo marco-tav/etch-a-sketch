c58a10b85152a36c48a1e258cb2c0c297b42be9e
marco-tav <marco.tavella@outlook.com>
2021-11-07 11:50:09 -0300

    Add button with clear screen function

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// Grid creation
 let n = 16;
 let amount = n**2;
 
@@ -9,11 +10,21 @@ for (let i = 0; i < amount; i++) {
     gridContainer.appendChild(cell);
 }
 
+// Grid changes color on mouseover
 const cells = document.querySelectorAll('.cell');
 
 cells.forEach((cell) => {
 
     cell.addEventListener('mouseover', () => {
         cell.classList.add('drawing');
+    })
+})
+
+// Clear function 
+const clearScreen = document.querySelector('.clear-screen');
+
+clearScreen.addEventListener('click', () => {
+    cells.forEach((cell) => {
+        cell.classList.remove('drawing');
     })
 })
