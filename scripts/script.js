fb4a93bc15fe971aab1b0abb6e9dbfd6b8ffbaf3
marco-tav <marco.tavella@outlook.com>
2021-11-11 10:52:33 -0300

    Add new createGrrid function and relevant styles. Now works for any grid size

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,28 @@
 //--------------------------- FUNCTION DECLARATIONS -----------------------------
 //-------------------------------------------------------------------------------
 
-// Here may go grid creation function
+function createGrid(cellsPerSide) {
+    const gridContainer = document.querySelector('.grid-container');
+
+    // Create a column of divs. Inside of each div will go a row of cells. 
+    for (let i = 0; i < cellsPerSide; i++) {
+        const cellContainer = document.createElement('div');
+        cellContainer.setAttribute('class', 'cell-container');
+        gridContainer.appendChild(cellContainer);
+    }
+
+    const rowContainers = document.querySelectorAll('.cell-container');
+
+    // Create a row of cells inside each div. This makes up the grid.
+    rowContainers.forEach((rowContainer) => {
+        for (let j = 0; j < cellsPerSide; j++) {
+            const cell = document.createElement('div');
+            cell.setAttribute('class', 'cell');
+            rowContainer.appendChild(cell);
+        }
+    })
+
+}
 
 function deleteGrid() {
     const gridContainer = document.querySelector('.grid-container');
@@ -14,13 +35,5 @@ function deleteGrid() {
 //------------------------------ EXECUTION --------------------------------------
 //-------------------------------------------------------------------------------
 
-const gridContainer = document.querySelector('.grid-container');
 
-let cellsPerSide = 50;
-
-for (let i = 0; i < cellsPerSide; i++) {
-    const cellContainer = document.createElement('div');
-    cellContainer.setAttribute('class', 'cell-container');
-    gridContainer.appendChild(cellContainer);
-}
 
